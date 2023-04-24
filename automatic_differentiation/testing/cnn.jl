c5e738d878f46317ecfd315eeb75535ec4bb8302
matlaczj <kuba.matlacz@outlook.com>
2023-04-24 18:44:15 +0000

    batches

--- automatic_differentiation/testing/cnn.jl
+++ automatic_differentiation/testing/cnn.jl
@@ -7,6 +7,7 @@ include("../broadcasted_operators.jl")
 include("../convolution.jl")
 using LinearAlgebra
 using Statistics
+using Plots
 
 function conv(w, b, x, activation)
 	out = conv(x, w) .+ b
@@ -22,38 +23,6 @@ function flatten(x)
 	return flatten(x)
 end
 
-function net(x, wh, bh, wo, bo, y)
-	x̂ = conv(wh, bh, x, relu) # out_channels, height_out, width_out, batch_size
-	x̂.name = "x̂"
-	x̂ = flatten(x̂)
-	x̂.name = "x̂"
-	ŷ = dense(wo, bo, x̂, logistic) # add activation like softmax
-	ŷ.name = "ŷ"
-	e = mean_squared_loss(y, ŷ)
-	e.name = "loss"
-	return topological_sort(e)
-end
-
-
-
-# for (i, n) in enumerate(graph)
-# 	if typeof(n) <: Variable
-# 		println("Node $i")
-# 		println(n.name)
-# 		println(n.output)
-# 		println(n.gradient)
-# 		println()
-# 	end
-# end
-
-# println("Forward pass")
-
-
-
-
-
-
-# function that adds gradient to the node's weight
 function update_weight!(node, learning_rate)
 	node.output -= learning_rate .* node.gradient
 end
@@ -70,80 +39,108 @@ function is_parameter(node)
 	return is_weight(node) || is_bias(node)
 end
 
+function is_x(node)
+	return occursin("x", node.name)
+end
+
+function is_y(node)
+	return occursin("y", node.name)
+end
+
 function has_name(node)
 	return hasproperty(node, :name)
 end
 
-# average out the gradient for biases to reduce its dimensionality
 function average_bias_gradient!(node)
 	node.gradient = mean(node.gradient, dims = (1, 2))
 end
 
-# run update on all weights and biases in the graph
-# except for the input node and the loss node
 function update_weights!(graph, learning_rate)
 	for (idx, node) in enumerate(graph)
 		if has_name(node) && is_parameter(node)
 			if is_bias(node)
 				average_bias_gradient!(node)
 			end
-			println(idx)
-			println(node.name)
 			update_weight!(node, learning_rate)
 		end
 	end
 end
 
-x = Variable(randn(8, 8, 1, 1), name = "x") # height, width, in_channels, batch_size
-wh = Variable(randn(3, 3, 1, 4), name = "wh") # height, width, in_channels, out_channels
-bh = Variable(randn(1, 1, 4, 1), name = "bh")
-wo = Variable(randn(6 * 6 * 4, 1), name = "wo")
-bo = Variable(randn(1, 1), name = "bo")
-y = Variable(randn(1), name = "y")
-
-graph = net(x, wh, bh, wo, bo, y)
-
-forward!(graph)
-backward!(graph)
-
-for (i, n) in enumerate(graph)
-	if typeof(n) <: Variable
-		println("Node $i")
-		println(n.name)
-		println(n.output)
-		println(size(n.output))
-		println(n.gradient)
-		println()
+function learning_iteration!(graph, learning_rate, if_print)
+	forward!(graph)
+	backward!(graph)
+	if if_print
+		for (i, n) in enumerate(graph)
+			if typeof(n) <: Variable
+				println("Node $i")
+				println(n.name)
+				println(n.output)
+				println(size(n.output))
+				println(n.gradient)
+				println()
+			end
+		end
 	end
-	# print(i, ". ")
-	# println(n)
+	update_weights!(graph, learning_rate)
 end
 
-update_weights!(graph, 0.1)
+function build_graph()
+	x = Variable(randn(8, 8, 1, 1), name = "x") # height, width, in_channels, batch_size
+	wh = Variable(randn(3, 3, 1, 4), name = "wh") # # height, width, in_channels, out_channels
+	bh = Variable(randn(1, 1, 4, 1), name = "bh")
+	wo = Variable(randn(6 * 6 * 4, 1), name = "wo")
+	bo = Variable(randn(1, 1), name = "bo")
+	y = Variable(randn(1), name = "y")
 
+	x̂ = conv(wh, bh, x, relu)
+	x̂.name = "x̂"
+	x̂ = flatten(x̂)
+	x̂.name = "x̂"
+	ŷ = dense(wo, bo, x̂, logistic)
+	ŷ.name = "ŷ"
+	e = mean_squared_loss(y, ŷ)
+	e.name = "loss"
+	return topological_sort(e), x, y
+end
 
+function train_model(x, y, learning_rate, n_iterations, if_print)
+	losses = Vector{Float64}()
+	graph, x_node, y_node = build_graph()
 
+	# total number of iterations is n_iterations * batch_size
+	for iter in 1:n_iterations
+		if iter % 100 == 0
+			println("Iteration $iter")
+		# iterate over 4th dimention of x
+		for i in 1:size(x, 4)
+			# println("Sample $i")
+			# current x as a 4d tensor
+			curr_x = x[:, :, :, i]
+			curr_x = reshape(curr_x, size(curr_x, 1), size(curr_x, 2), size(curr_x, 3), 1)
+			# current y as a vector
+			curr_y = y[i, :]
+			# replace x and y nodes output
+			x_node.output = curr_x
+			y_node.output = curr_y
+			# run learning iteration
+			learning_iteration!(graph, learning_rate, if_print)
+			# get loss
+			loss = graph[end].output[1]
+			push!(losses, loss)
+		end
+	end
+	return losses
+end
 
-graph[6]
-
-graph[6].output
-
-graph[6].gradient
-
-graph[4]
-
-graph[4].output
-
-graph[4].gradient
-
-graph[5]
-
-graph[5].output
-
-graph[5].gradient
-
-graph[6].output # add bias to each in_channel
-
-graph[3].output .+ randn(1)
-
-average_bias_gradient!(graph[6])
+n_samples = 100
+height = 8
+width = 8
+channels = 1
+x = randn(height, width, channels, n_samples)
+y = randn(n_samples)
+n_iterations = 1000
+learning_rate = 0.1
+losses = train_model(x, y, learning_rate, n_iterations, false)
+println(losses)
+# visualize loss
+plot(losses, title = "Loss", xlabel = "Iteration", ylabel = "Loss")
