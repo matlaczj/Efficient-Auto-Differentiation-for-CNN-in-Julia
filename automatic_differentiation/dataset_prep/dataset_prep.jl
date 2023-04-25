4becd41f1bc97a45ed79213cf5148064f786d021
matlaczj <kuba.matlacz@outlook.com>
2023-04-25 08:54:07 +0000

    batch gradient update and dataset loading

--- automatic_differentiation/dataset_prep/dataset_prep.jl
+++ automatic_differentiation/dataset_prep/dataset_prep.jl
@@ -1,42 +1,7 @@
-using MLDatasets
-
-train_x, train_y = MNIST.traindata()
-test_x, test_y = MNIST.testdata()
-
-# Filter out images that are not of the digit 5
-train_mask = train_y .== 5
-test_mask = test_y .== 5
-train_x = train_x[:, :, train_mask]
-test_x = test_x[:, :, test_mask]
-
-using Images
-
-# Rescale images to 8x8
-train_x = map(img -> imresize(img, (8, 8)), train_x)
-test_x = map(img -> imresize(img, (8, 8)), test_x)
-
-train_x = reshape(train_x, (8, 8, 1, :))
-test_x = reshape(test_x, (8, 8, 1, :))
-
-# Stack images into batches with batch_size 1
-train_x = cat(train_x..., dims = 4)
-test_x = cat(test_x..., dims = 4)
-
-train_x = Gray.(train_x)
-test_x = Gray.(test_x)
-
-train_y = train_y[train_mask] .== 5
-test_y = test_y[test_mask] .== 5
-
-using Random
-
-# Find the class with fewer samples
-minority_class = findfirst(train_y .== 1)
-majority_class = findfirst(train_y .== 0)
-
-# Undersample the majority class
-n_minority = sum(train_y)
-n_majority = length(train_y) - n_minority
-rand_idxs = randperm(n_majority)[1:n_minority]
-train_x = cat(train_x[:, :, minority_class, :], train_x[:, :, majority_class, rand_idxs], dims = 4)
-train_y = vcat(fill(true, n_minority), fill(false, n_minority))
+using MLDatasets: MNIST
+train_ds = MNIST(:train)
+test_ds = MNIST(:test)
+x_train = train_ds.features
+y_train = train_ds.targets
+x_test = test_ds.features
+y_test = test_ds.targets
--- automatic_differentiation/testing/cnn.jl
+++ automatic_differentiation/testing/cnn.jl
@@ -8,6 +8,8 @@ include("../convolution.jl")
 using LinearAlgebra
 using Statistics
 using Plots
+using MLDatasets: MNIST
+using Images
 
 function conv(w, b, x, activation)
 	out = conv(x, w) .+ b
@@ -66,6 +68,17 @@ function update_weights!(graph, learning_rate)
 	end
 end
 
+function save_param_gradients!(graph, gradients_in_batch)
+	for (idx, node) in enumerate(graph)
+		if has_name(node) && is_parameter(node)
+			if is_bias(node)
+				average_bias_gradient!(node)
+			end
+			push!(gradients_in_batch[node.name], node.gradient)
+		end
+	end
+end
+
 function learning_iteration!(graph, learning_rate, if_print)
 	forward!(graph)
 	backward!(graph)
@@ -81,14 +94,18 @@ function learning_iteration!(graph, learning_rate, if_print)
 			end
 		end
 	end
-	update_weights!(graph, learning_rate)
+	# update_weights!(graph, learning_rate)
 end
 
 function build_graph()
-	x = Variable(randn(8, 8, 1, 1), name = "x") # height, width, in_channels, batch_size
-	wh = Variable(randn(3, 3, 1, 4), name = "wh") # # height, width, in_channels, out_channels
-	bh = Variable(randn(1, 1, 4, 1), name = "bh")
-	wo = Variable(randn(6 * 6 * 4, 1), name = "wo")
+	input_size = 28
+	kernel_size = 3
+	input_channels = 1
+	out_channels = 4
+	x = Variable(randn(input_size, input_size, input_channels, 1), name = "x") # height, width, in_channels, batch_size
+	wh = Variable(randn(kernel_size, kernel_size, input_channels, out_channels), name = "wh") # # height, width, in_channels, out_channels
+	bh = Variable(randn(1, 1, out_channels, 1), name = "bh")
+	wo = Variable(randn((input_size-2) * (input_size-2) * out_channels, 1), name = "wo")
 	bo = Variable(randn(1, 1), name = "bo")
 	y = Variable(randn(1), name = "y")
 
@@ -104,16 +121,17 @@ function build_graph()
 end
 
 function train_model(x, y, learning_rate, n_iterations, if_print)
-	losses = Vector{Float64}()
 	graph, x_node, y_node = build_graph()
-
+	avg_losses = Vector{Float64}()
+	
 	# total number of iterations is n_iterations * batch_size
 	for iter in 1:n_iterations
-		if iter % 100 == 0
-			println("Iteration $iter")
+		losses_in_batch = Vector{Float64}()
+		gradients_in_batch = Dict{String, Vector{Float64}}()
+		println("Iteration $iter")
 		# iterate over 4th dimention of x
 		for i in 1:size(x, 4)
-			# println("Sample $i")
+			println("Sample $i")
 			# current x as a 4d tensor
 			curr_x = x[:, :, :, i]
 			curr_x = reshape(curr_x, size(curr_x, 1), size(curr_x, 2), size(curr_x, 3), 1)
@@ -124,23 +142,50 @@ function train_model(x, y, learning_rate, n_iterations, if_print)
 			y_node.output = curr_y
 			# run learning iteration
 			learning_iteration!(graph, learning_rate, if_print)
+			# save parameter gradients
+			save_param_gradients!(graph, gradients_in_batch)
 			# get loss
 			loss = graph[end].output[1]
-			push!(losses, loss)
+			push!(losses_in_batch, loss)
 		end
+		# update parameters
+		for (key, value) in gradients_in_batch
+			gradients_in_batch[key] = mean(gradients_in_batch[key], dims = 1)
+		end
+		for (idx, node) in enumerate(graph)
+			if has_name(node) && is_parameter(node)
+				node.output -= learning_rate .* gradients_in_batch[node.name]
+			end
+		end
+		# get average loss
+		avg_loss = mean(losses_in_batch)
+		push!(avg_losses, avg_loss)
 	end
 	return losses
 end
 
-n_samples = 100
-height = 8
-width = 8
-channels = 1
-x = randn(height, width, channels, n_samples)
-y = randn(n_samples)
-n_iterations = 1000
-learning_rate = 0.1
+train_ds = MNIST(:train)
+x_train = train_ds.features
+y_train = train_ds.targets
+x_train = reshape(x_train, size(x_train, 1), size(x_train, 2), 1, size(x_train, 3))
+
+# modify target 'y_train' to be binary classification of 5 or not 5
+y_train = y_train .== 5
+
+# take only 100 first samples of train datasets
+x_train = x_train[:, :, :, 1]
+y_train = y_train[1, :]
+x = x_train
+y = y_train
+
+# n_samples = 2
+# height = 8
+# width = 8
+# channels = 1
+# x = randn(height, width, channels, n_samples)
+# y = randn(n_samples)
+n_iterations = 10
+learning_rate = 0.01
 losses = train_model(x, y, learning_rate, n_iterations, false)
-println(losses)
 # visualize loss
 plot(losses, title = "Loss", xlabel = "Iteration", ylabel = "Loss")
